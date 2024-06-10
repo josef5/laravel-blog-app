734082019adca5884759271bbf283fdd7b8ef51e
Jose Espejo <joseespejo@yahoo.com>
2024-06-10 15:16:08 +0100

    refactor: routes to remove ExampleController

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ExampleController;
 use App\Http\Controllers\UserController;
 
-Route::get('/', [ExampleController::class, 'homepage']);
-Route::get('/about', [ExampleController::class, 'about']);
+Route::get('/', [UserController::class, 'showCorrectHomepage']);
 
 Route::post('/register', [UserController::class, 'register']);
+Route::post('/login', [UserController::class, 'login']);
+Route::post('/logout', [UserController::class, 'logout']);
