4dcde271a949297cdea8bd1903b8864fee040f45
Jose Espejo <joseespejo@yahoo.com>
2024-06-13 15:19:59 +0100

    feat: Add Post model and migration
    This commit adds the `Post` model and migration files. The `Post` model includes a `user` relationship and defines the fillable attributes. The migration file creates the `posts` table with the necessary columns. This is a feature addition to support creating and displaying posts in the application.

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,20 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
-Route::get('/', [UserController::class, 'showCorrectHomepage']);
+// Auth routes
+Route::get('/', [UserController::class, 'showCorrectHomepage'])->name('login');
+Route::post('/register', [UserController::class, 'register'])->middleware('guest');
+Route::post('/login', [UserController::class, 'login'])->middleware('guest');
+Route::post('/logout', [UserController::class, 'logout'])->middleware('mustBeLoggedIn');
 
-Route::post('/register', [UserController::class, 'register']);
-Route::post('/login', [UserController::class, 'login']);
-Route::post('/logout', [UserController::class, 'logout']);
+// Blog routes
+Route::get('/create-post', [PostController::class, 'showCreateForm'])->middleware('mustBeLoggedIn');
+Route::post('/create-post', [PostController::class, 'storeNewPost'])->middleware('mustBeLoggedIn');
+Route::get('/post/{post}', [PostController::class, 'viewSinglePost']);
+
+// Profile routes
+Route::get('/profile/{user}', [UserController::class, 'showProfile'])->middleware('mustBeLoggedIn');
+Route::post('/profile/{user}', [UserController::class, 'updateProfile'])->middleware('mustBeLoggedIn');
