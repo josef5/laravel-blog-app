3ac77b9a4116584b8bf982a3d05c9f154ffff859
Jose Espejo <joseespejo@yahoo.com>
2024-06-20 14:18:40 +0100

    feat: Add routes for managing user avatars and following/unfollowing users
    This commit adds new routes for managing user avatars and following/unfollowing users. The `/manage-avatar` route allows users to view and update their avatar image, while the `/create-follow/{user:username}` and `/remove-follow/{user:username}` routes enable users to follow or unfollow other users. These routes are essential for enhancing user profiles and social interactions within the application.

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,21 @@
 <?php
 
+use App\Http\Controllers\FollowController;
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
-// Auth routes
+// User routes
 Route::get('/', [UserController::class, 'showCorrectHomepage'])->name('login');
 Route::post('/register', [UserController::class, 'register'])->middleware('guest');
 Route::post('/login', [UserController::class, 'login'])->middleware('guest');
 Route::post('/logout', [UserController::class, 'logout'])->middleware('mustBeLoggedIn');
+Route::get('/manage-avatar', [UserController::class, 'showAvatarForm'])->middleware('mustBeLoggedIn');
+Route::post('/manage-avatar', [UserController::class, 'storeAvatar'])->middleware('mustBeLoggedIn');
+
+// Follow routes
+Route::post('/create-follow/{user:username}', [FollowController::class, 'createFollow'])->middleware('mustBeLoggedIn');
+Route::post('/remove-follow/{user:username}', [FollowController::class, 'removeFollow'])->middleware('mustBeLoggedIn');
 
 // Blog routes
 Route::get('/create-post', [PostController::class, 'showCreateForm'])->middleware('mustBeLoggedIn');
@@ -20,6 +27,8 @@ Route::put('/post/{post}', [PostController::class, 'actuallyUpdate'])->middlewar
 
 // Profile routes
 Route::get('/profile/{user:username}', [UserController::class, 'showProfile']); //->middleware('mustBeLoggedIn');
+Route::get('/profile/{user:username}/followers', [UserController::class, 'showProfileFollowers'])->middleware('mustBeLoggedIn');
+Route::get('/profile/{user:username}/following', [UserController::class, 'showProfileFollowing'])->middleware('mustBeLoggedIn');
 Route::post('/profile/{user}', [UserController::class, 'updateProfile'])->middleware('mustBeLoggedIn');
 
 // Admin routes
